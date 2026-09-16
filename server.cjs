24af5c0e0c3a527bc5214ada6bf9d77dfb588fb8
Roshni <kushwaharoshni321@gmail.com>
2026-09-16 10:47:25 +0530

    Fix JSON server database path

--- server.cjs
+++ server.cjs
@@ -1,7 +1,11 @@
+const path = require("path");
 const jsonServer = require("json-server");
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+
+const dbPath = path.join(__dirname, "db.json");
+
+const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3001;
